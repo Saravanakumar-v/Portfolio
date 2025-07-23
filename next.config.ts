1c09697e5b3a305146bec0d64a9bd94ab5f6b6af
Saravana Kumar <saravanakumar.v@samco.in>
2025-07-23 23:16:27 +0530

    Config changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+  output: "export", // This enables static export with `next export`
+  basePath: isProd ? "/your-repo-name" : "",
+  assetPrefix: isProd ? "/your-repo-name/" : "",
+  images: {
+    unoptimized: true, // Required if using next/image with static export
+  },
+};
 
 export default nextConfig;
