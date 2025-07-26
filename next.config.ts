f7a165edcff0fd87b38cf8e2eb872f38735a0ea6
Saravana Kumar <saravanakumar.v@samco.in>
2025-07-26 13:14:51 +0530

    config changes.

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: "export", // This enables static export with `next export`
-  basePath: isProd ? "/your-repo-name" : "",
-  assetPrefix: isProd ? "/your-repo-name/" : "",
-  images: {
-    unoptimized: true, // Required if using next/image with static export
-  },
+  output: "export", 
 };
 
 export default nextConfig;
