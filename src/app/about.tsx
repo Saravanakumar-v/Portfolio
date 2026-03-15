f5964ce0348e3b311727908b7dd3614757f34774
Saravana Kumar <saravanakumar.v@samco.in>
2026-03-15 22:04:18 +0530

    Update

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -10,7 +10,7 @@ export default function About() {
     <div className='about flex flex-col gap-[15px]'>
       <div className='font-bold text-[18px] flex items-center gap-[5px]'><InfoOutlineRoundedIcon />About Me</div>
       <div className='content text-normal'>
-          Web Developer with <b>2 years of experiences in React.js</b>, specializing in front-end UI development for web applications. Proficient in redesigning user interfaces, creating responsive designs and ensuring cross-browser compatibility.
+        Web developer with 2+ years of experience specializing in frontend and full-stack development. Proficient in React.js, TypeScript, and modern web technologies, with a strong focus on building scalable, performant, and responsive applications.
       </div>
     </div>
     </>
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
@@ -9,8 +9,12 @@ import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded
 import './globals.scss';
 import BusinessRoundedIcon from '@mui/icons-material/BusinessRounded';
 import { useToggle } from './toggleContext';
+import { ContentCopyRounded } from '@mui/icons-material';
+import { Snackbar } from '@mui/material';
 
 export default function Navbar() {
+
+  const [showSnackbar, setShowSnackbar] = useState(false);
 
   const handleNavigation = (type: string) => {
     switch (type) {
@@ -28,6 +32,10 @@ export default function Navbar() {
         break;
       case 'college':
         window.open('https://www.svce.ac.in/','_blank');
+        break;
+      case 'mailto': 
+        window.navigator.clipboard.writeText('saravanakumar.cs23@gmail.com');
+        setShowSnackbar(true);
         break;
       default:
         break;
@@ -49,23 +57,26 @@ export default function Navbar() {
             {toggle == 'exp' ?
             <>
               <div className='opacity-[0.7] flex items-end gap-[5px] underline cursor-pointer' onClick={() => handleNavigation('work')}><BusinessRoundedIcon />SAMCO Securities Limited</div>
-              <div className='opacity-[0.7]'>Junior Web Developer</div>
+              <div className='opacity-[0.7] text-[14px]'>Software Engineer | React.js | <br/>Web Development </div>
             </>
             :
              <>
               <div className='opacity-[0.7] flex items-end gap-[5px] underline cursor-pointer' onClick={() => handleNavigation('college')}><AccountBalanceRoundedIcon />Sri Venkateswara College</div>
-              <div className='opacity-[0.7]'>B.E, Computer Science & Engineering</div>
+              <div className='opacity-[0.7] text-[14px]'>B.E, Computer Science & Engineering</div>
             </>
             }
         </div>
-        <div className='text-[16px]'>📍 Thiruvallur, Chennai</div>
+        <div className='text-[14px]'>📍 {toggle == 'exp' ?'Thiruvallur, Chennai' :'Thandalam, Sriperumbathur'}</div>
         <div className='flex items-center gap-[10px] text-[16px] cursor-pointer contact-info'  style={{transition: 'width 0.5s'}}> 
           {toggle == 'exp' &&
             <a className='flex items-center gap-[5px] contact-resume' href='/Saravanakumar_resume.pdf' download={'Saravanakumar_resume'} ><DownloadRoundedIcon />Resume</a>
           }
           <div className='cursor-pointer' onClick={() => handleNavigation('linkedin')}><LinkedInIcon /></div>
           <div className='cursor-pointer' onClick={() => handleNavigation('github')}><GitHubIcon className='github'/></div>
-          <div><EmailRoundedIcon /></div>
+          <div className='cursor-pointer' onClick={() => handleNavigation('mailto')}><EmailRoundedIcon /></div>
+          <Snackbar open={showSnackbar} autoHideDuration={3000} onClose={() => setShowSnackbar(false)} anchorOrigin={{vertical: 'bottom', horizontal: 'center'}} className='animate__animated animate__slideInUp'>
+            <div className={`flex items-center gap-[5px] ${toggle == 'exp' ?'bg-white text-black' :'bg-black text-white'} p-[12px] rounded-[14px]`}>Copied to Clipboard <ContentCopyRounded style={{width: '20px',height: '20px',color: toggle == 'exp' ?'black' :'white'}}/></div>
+          </Snackbar>
         </div>
     </nav>
   )
