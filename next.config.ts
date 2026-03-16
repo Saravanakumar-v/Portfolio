5ba3fb8254fbf369ab991241c3e8ad2fffab9ac6
Saravana Kumar <saravanakumar.v@samco.in>
2026-03-16 23:12:05 +0530

    g

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: "export", 
-  images: {
-    unoptimized: true, // Required if using next/image with static export
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
